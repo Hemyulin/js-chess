b9e177612a1393f485a412f32597f98048f2f437
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-23 16:29:53 +0100

    new try withcreateChessBoard...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-function createBoard() {
-  array.forEach((element) => {
-    document.createElement("div");
-  });
+function createChessBoard() {
+  const board = document.createElement("div");
+  board.className = "board";
+
+  for (let row = 0; row < 8; row++) {}
 }
+
+createChessBoard();
