9796e7bc63304f89ca0573f1f581789be4aa67d9
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-23 16:51:21 +0100

    rows are visible on screen

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 function createChessBoard() {
   const board = document.createElement("div");
-  board.className = "board";
+  board.className = "chess-board";
 
   for (let row = 0; row < 8; row++) {
     const rowDiv = document.createElement("div");
     rowDiv.className = "row";
-    for (let column = 0; column < 8; column++) {
-      const columnDiv = document.createElement(div);
-      columnDiv.className = column;
-    }
+    console.log("rowDiv");
+    board.appendChild(rowDiv);
   }
+  document.body.appendChild(board);
+  console.log(board);
 }
 
 createChessBoard();
+console.log();
