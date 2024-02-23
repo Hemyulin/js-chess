b2889c83dcd519b31d11665c141a9d258ff22045
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-23 16:33:28 +0100

    trying to create rows and columns

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ function createChessBoard() {
   const board = document.createElement("div");
   board.className = "board";
 
-  for (let row = 0; row < 8; row++) {}
+  for (let row = 0; row < 8; row++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "row";
+    for (let column = 0; column < 8; column++) {
+      const columnDiv = document.createElement(div);
+      columnDiv.className = column;
+    }
+  }
 }
 
 createChessBoard();
