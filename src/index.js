a46aeb0b443bf9f0d6ea416105fe95eafe063106
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-26 12:59:58 +0100

    got to show right number on square

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ function createChessBoard() {
   const board = document.createElement("div");
   board.className = "chess-board";
 
-  for (let row = 0; row < 8; row++) {
+  for (let row = 8; row > 0; row--) {
     const rowDiv = document.createElement("div");
     rowDiv.className = "row";
     // rowDiv.
     for (let column = 0; column < 8; column++) {
       const square = document.createElement("div");
       square.className = "square";
-      square.innerText = `col${column + 1} row${row + 1}`;
+      square.innerText = `${String.fromCharCode(column + 97)}${row}`;
       if ((row + column) % 2 === 0) {
         square.classList.add("light");
       } else {
