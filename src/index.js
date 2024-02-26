a515f9ae22d65d961aa531be8fbd62975d82aa04
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-26 13:25:41 +0100

    got to put a column of white pawns. This is progress. Next up: a row of pawns instead

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function createChessBoard() {
     for (let column = 0; column < 8; column++) {
       const square = document.createElement("div");
       square.className = "square";
+      // assign each square a unique id
       square.id = `square-${String.fromCharCode(column + 97)}${row}`;
       if ((row + column) % 2 === 0) {
         square.classList.add("light");
@@ -25,10 +26,25 @@ function createChessBoard() {
 
 createChessBoard();
 
+function initialBoardPositions() {
+  let whitePawns = Array(8)
+    .fill()
+    .map(() => new Pawn("white", document.createElement("img")));
+
+  let counter = 1;
+
+  whitePawns.forEach((element) => {
+    let square = document.getElementById(`square-c${counter}`);
+    square.appendChild(element.figureImage);
+    counter += 1;
+  });
+
+  for (let i = 97; i < 105; i++) {
+    whitePawns[i - 97] = new Pawn("white", document.createElement("img"));
+    let square = document.getElementById(`square-c${i - 97}`);
+  }
+}
+
+initialBoardPositions();
+
 // TODO: ADD WHO'S TURN
-
-let whitePawns = Array(8)
-  .fill()
-  .map(() => new Pawn("white", document.createElement("img")));
-
-console.log(whitePawns);
